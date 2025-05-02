557382f7f97bfd7844855701743eb0312521d5b9
JulianySavazzi <julianysaz@gmail.com>
2025-05-02 20:03:32 -0300

    feature - to do routes

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,10 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::prefix('/user')->group(function () {
         Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
     });
+    Route::prefix('/chat/messages')->group(function () {
+        Route::get('/{user-to}',[]); //TODO listar mensagens (conversas do auth user com outro user) - id-ser
+        Route::post('/{user-to}', []);//TODO enviar mensagem para outro user
+        Route::get('/team/{team}',[]); //TODO listar mensagens da team que estou - id-team
+        Route::post('/team/{team}', []);//TODO enviar mensagem na team
+    });
 });
